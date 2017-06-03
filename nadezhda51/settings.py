5c87c8622fb0a5d6615c1c7da5863de066979e40
Denis <denisyurievich94@gmail.com>
2017-06-03 18:18:43 +0300

    update

--- nadezhda51/settings.py
+++ nadezhda51/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_URL = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 THUMBNAIL_DEBUG=True
